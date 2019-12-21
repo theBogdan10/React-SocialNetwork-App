1eb8d0390e0b3e0a7c83b56abf5e14d955cba306
Bogdan <bybogdan@mail.ru>
2019-12-21 14:15:08 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
 import Dialogs from './components/Dialogs/Dialogs'
 
+import { BrowserRouter,Route } from "react-router-dom"
+
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header />
-            <Navbar /> 
-            <div className="app-wrapper-content"> 
-            <Dialogs />
+            <Navbar />
+            <div className="app-wrapper-content">
+                <Route path="/profile" component={Profile} />
+                <Route path="/dialogs" component={Dialogs} />
+                {/* <Route path="/news" component={News} />
+                <Route path="/music" component={Music} />
+                <Route path="/settings" component={Settings} /> */}
             </div>
-            
-           
         </div>
+        </BrowserRouter>
     );
 }
 export default App;
