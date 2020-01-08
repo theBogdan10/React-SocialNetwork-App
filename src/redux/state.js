5c7a7adeadb6ebb08eeb004947b94ee8beddf87e
Bogdan <bybogdan@mail.ru>
2020-01-08 14:05:26 +0300

    added dispatch method

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,63 +1,65 @@
-let store={
-	_state:{
-		profilePage:{
-			posts:[
-				{id:1,message:'Hi, how are you ?',likesCount:20},
-				{id:2,message:'Its my first post',likesCount:15},
-				{id:3,message:'She really busy',likesCount:50}  
+/* eslint-disable indent */
+/* eslint-disable no-case-declarations */
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{ id: 1, message: 'Hi, how are you ?', likesCount: 20 },
+				{ id: 2, message: 'Its my first post', likesCount: 15 },
+				{ id: 3, message: 'She really busy', likesCount: 50 }
 			],
-			newPostText:'Hello'
+			newPostText: 'Hello'
 		},
-		
-		dialogsPage:{
-			dialogs : [
+
+		dialogsPage: {
+			dialogs: [
 				{ id: 1, name: 'Bogdan' },
 				{ id: 2, name: 'Hamza' },
 				{ id: 3, name: 'Misha' },
 				{ id: 4, name: 'Lesha' },
 				{ id: 5, name: 'Vovav' }
 			],
-		
-			messages : [
+
+			messages: [
 				{ id: 1, message: 'Hellloooooo' },
 				{ id: 2, message: 'How are you?' },
 				{ id: 3, message: 'Fine bro' }
-			],  
+			],
 		},
-		sideBar:{	
-		}   
+		sideBar: {
+		}
 	},
 
-	getState(){
+	_callSubscr() {
+		console.log('State changed');
+	},
+
+	getState() {
 		return this._state;
 	},
 
-	_callSubscr(){
-		console.log('State changed');
-
-	},
-	
-	addPost(){
-		let newPost={
-			id:5,
-			message:this._state.profilePage.newPostText,
-			likesCount:0
-		};
-		this._state.profilePage.posts.push(newPost);
-		this._state.profilePage.newPostText='';
-		this._callSubscr(this._state);
-	
+	subscribe(observer) {
+		this._callSubscr = observer;
 	},
 
-	updateNewPostText(newText){
-		this._state.profilePage.newPostText=newText;
-		this._callSubscr(this._state);
-	},
-
-	subscribe(observer){
-		this._callSubscr=observer;
-	},
-	
+	dispatch(action) {
+		switch (action.type) {
+			case 'ADD-POST':
+				let newPost = {
+					id: 5,
+					message: this._state.profilePage.newPostText,
+					likesCount: 0
+				};
+				this._state.profilePage.posts.push(newPost);
+				this._state.profilePage.newPostText = '';
+				this._callSubscr(this._state);
+				break;
+			case 'UPDATE-NEW-POST-TEXT':
+				this._state.profilePage.newPostText =action.newText;
+				this._callSubscr(this._state);
+				break;
+		}
+	}
 };
 
 export default store;
